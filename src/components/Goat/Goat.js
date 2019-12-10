b46f4283623ef544c033e2bd3d00e2d3dfe673d6
Connor Sullivan <cksullvn@gmail.com>
2019-12-09 20:44:26 -0600

    created proptypes for goats

--- src/components/Goat/Goat.js
+++ src/components/Goat/Goat.js
@@ -4,7 +4,7 @@ class Goat extends React.Component {
   render() {
     const { goat } = this.props;
     return (
-      <div className="card col-3">
+      <div className="card">
         <img className="card-img-top" src={goat.imgUrl} alt="Card cap"/>
         <div className="card-body">
           <h5 className="card-title">{goat.name}</h5>
--- src/components/GoatCoral/GoatCoral.js
+++ src/components/GoatCoral/GoatCoral.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Goat from '../Goat/Goat';
+import goatShape from '../../helpers/propz/goatShape';
 
 class GoatCoral extends React.Component {
+  static propTypes = {
+    goats: PropTypes.arrayOf(goatShape.goatShape),
+  }
+
   render() {
     const myGoats = this.props.goats;
     const goatCards = myGoats.map((goat) => <Goat key={goat.id} goat={goat}/>);
